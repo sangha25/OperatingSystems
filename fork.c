51cd6274a28b58ef9bafa653d4da9f248718a471
sangha25 <sanghapal25@gmail.com>
2018-10-29 18:17:53 +0530

    Update fork.c

--- fork.c
+++ fork.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int pid;
+pid=fork();
+if(pid>0)
+{
+ printf("\n child : %d",getpid());
+}
+else if(pid==0)
+{
+printf("\n parent : %d",getpid());
+printf("\n parent : %d",getppid());
+}
+}
 
