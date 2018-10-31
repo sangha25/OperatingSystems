f374351f690f27e9a797157c0ae0b93c34170982
sangha25 <sanghapal25@gmail.com>
2018-10-31 12:14:27 +0530

    code improved

--- Cmd.sh
+++ Cmd.sh
@@ -1,1 +1,23 @@
+#!/bin/sh
+#the shell program for CLA
+echo "The name of the script file is $0"
+
+# display total number of arguments passed to the script
+echo "Total number of arguments passed to the script = $#"
+
+# display all the arguments using for loop
+if [ $# -gt 0 ]
+then
+  
+  echo "List of arguments:"
+  for arg in $@
+  do
+    echo "$arg"
+  done
+
+else
+  
+  echo "No argument provided to the script."
+
+Fi
 
