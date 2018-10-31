b94505251948879daba27a98dc52717c67c21ebd
sangha25 <sanghapal25@gmail.com>
2018-10-31 12:12:12 +0530

    Code Imroved
    Menu Driven Program in shell scripting.

--- menudriven.sh
+++ menudriven.sh
@@ -1,1 +1,25 @@
-
+while :
+do
+ clear
+ echo "Select Any Option To Proceed."
+ echo "1. Contents of /etc/passwd"
+ echo "2. List of users currently logged"
+ echo "3. Prsent handling directory"
+ echo "4. Exit"
+ echo -n "Please enter option [1 - 4]"
+ read opt
+ case $opt in
+  1) echo "************ Conents of /etc/passwd *************";
+     more /etc/passwd;;
+  2) echo "*********** List of users currently logged";
+     who | more;;
+  3) echo "You are in $(pwd) directory";   
+     echo "Press [enter] key to continue. . .";
+     read enterKey;;
+  4) echo "Bye $USER";
+     exit 1;;
+  *) echo "$opt is an invaild option. Please select option between 1-4 only";
+     echo "Press [enter] key to continue. . .";
+     read enterKey;;
+esac
+done
