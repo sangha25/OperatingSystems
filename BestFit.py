da2d063faf58eb8183e8c41cde5d298971c63932
sangha25 <sanghapal25@gmail.com>
2018-10-31 12:27:55 +0530

    Update BestFit.py
    Memory Allocation Methods in OS

--- BestFit.py
+++ BestFit.py
@@ -1,1 +1,15 @@
+blockSize =list(map(int,input('Enter Block Sizes: ').split()))
+processes = list(map(int,input('Enter Proccesses: ').split()))
+sort = sorted(blockSize)
+Result = []
+for i in range(len(processes)):
+    for j in range(len(blockSize)):
+        if sort[j] >= processes[i]:
+            Result.append([i,blockSize.index(sort[j])])
+            sort[j]-=processes[i]
+            break
+print('Process No\t\tProcess Size\t\t Block No.\t')
+for x,y in Result:
+    print(' ',x+1,'\t\t\t',processes[x],'\t\t\t',y+1)
+print('\nRemaining Processes if any Are not Allocated to any block') 
 
